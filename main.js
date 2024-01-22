79dcfe587a218ef44e6675696c8e73fbc9f6cd8f
Farhan Siddiq <157281829+farhansiddiq16@users.noreply.github.com>
2024-01-22 23:23:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+let user = await inquirer.prompt([
+    {
+        type: "string",
+        name: "cardholdername",
+        message: "Welcome Farhan Siddq"
+    },
+    {
+        type: "number",
+        name: "pinCode",
+        message: "Enter your 4 digit pin code"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        message: "select your account type",
+        choices: ["current", "saving"]
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        message: "Select your Transaction Type",
+        choices: ["fast cash", "cash withdrawl"]
+    },
+    {
+        type: "list",
+        name: "amount",
+        message: "Select your Amount",
+        choices: [1000, 2000, 3000, 5000, 10000],
+        when(user) {
+            return user.transactionType === "fast cash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your Amount",
+        when(user) {
+            return user.transactionType === "cash withdrawl";
+        }
+    }
+]);
+if (user.pinCode) {
+    const balance = Math.floor(Math.random() * 100000 + 1);
+    console.log(balance);
+    const enteredamount = user.amount;
+    if (enteredamount <= balance) {
+        const remaining = balance - enteredamount;
+        console.log(`You have withdrawn Rupees ${enteredamount} and your remaining balance is ${remaining}`);
+    }
+}
